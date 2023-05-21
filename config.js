a7bb41aadaee6a6dd67f60fa0f62042bdbd46103
Tittanbot <134238641+Tittanbot@users.noreply.github.com>
2023-05-21 20:17:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  [ '559181614398' , 'titan' , verdadeiro ],
+  [ '6281255369012' , 'Ash' , verdadeiro ],
   [''] 
 ] //Number of owners
 
